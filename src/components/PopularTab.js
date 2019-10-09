2cd1ad2524e3600e411002919df5aa9a273dec8c
dingping <1525612229@qq.com>
2019-10-09 11:26:07 +0800

    解决点击安卓物理返回键退出程序的问题

--- src/components/PopularTab.js
+++ src/components/PopularTab.js
@@ -1,11 +1,29 @@
 import React, { Component, Fragment } from 'react'
-import { StyleSheet, View, Text} from 'react-native'
+import { StyleSheet, View, Text,BackHandler} from 'react-native'
 import NavigatorUtil from '../navigator/NavigatorUtil'
+import {connect} from 'react-redux'
+import { NavigationActions} from 'react-navigation'
 
-export default class PopularTab extends Component {
+class PopularTab extends Component {
 	constructor(props) {
 		super(props);
 	}
+	componentDidMount() {
+		console.log('nav', this.props.nav)
+		BackHandler.addEventListener('hardwareBackPress', this.onBackPress)
+	}
+	componentWillUnmount() {
+		BackHandler.removeEventListener('hardwareBackPress', this.onBackPress)
+	}
+	onBackPress = () => {
+		const { navigation: { dispatch }, nav } = this.props
+		if (nav.routes[1].index === 0) {
+			return false
+		}
+		dispatch(NavigationActions.back())
+		return true
+	}
+	
 	render() {
 		return (
 			<View style={styles.container}>
@@ -15,7 +33,13 @@ export default class PopularTab extends Component {
 		);
 	}
 }
+const mapStateToProps = state => ({
+	nav: state.nav
+})
+const mapDispatchToProps = dispatch => ({
 
+})
+export default connect(mapStateToProps, mapDispatchToProps)(PopularTab)
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
